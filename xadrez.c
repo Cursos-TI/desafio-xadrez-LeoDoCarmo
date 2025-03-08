04590ac46b55eb835836925db35021689ff86a21
LeoDoCarmo <202401237906@alunos.estacio.br>
2025-03-08 00:39:59 +0000

    Desafio: nível aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-
+int main() {
     // Torre: movimento horizontal para a direita (5 casas)
     printf("Movimento da Torre:\n");
     for (int i = 0; i < 5; i++) {
@@ -26,7 +24,16 @@ int main()
         k++;
     } while (k < 8);
 
-
+    // Movimento do Cavalo: 2 casas para baixo, 1 casa para a esquerda
+    printf("\n\nMovimento do Cavalo:\n");
+    for (int baixo = 0; baixo < 2; baixo++) {
+        printf("Baixo\n");
+    }
+    int esquerda = 0;
+    while (esquerda < 1) {
+        printf("Esquerda\n");
+        esquerda++;
+    }
 
     return 0;
 }
